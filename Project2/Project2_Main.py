0b7ba053186cf59d47c2b7a634dac956d961f0a0
czjined <czjined@gmail.com>
2021-09-10 14:52:04 +0200

    Hotovo, v.0 - zbyva export pokusu do CSV.

--- Project2/Project2_Main.py
+++ Project2/Project2_Main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def main():
     bulls, cows = 0, 0
@@ -8,16 +9,41 @@ def main():
     print("I've generated a random 4 digit number for you.")
     print("Let's play a bulls and cows game.")
     oddeleni_textu()
-    hidden_number = random.sample(range(1,10), k=1) + random.choices(range(10), k=3)
+    hidden_number = generator_cisla()
     print(hidden_number)
+    start_time = time.time()
     while cows < 4:
+        bulls, cows = 0, 0
         input_number = list(input('Enter a number: '))
+        print(input_number)
+        pokusy += 1
         if kontrola_vstupu(input_number):
-            print("Zadani v poradku, zahajuji vyhodnocovani.")
-            cows += 1
+            for i, cislo in enumerate(input_number):
+                if cislo == hidden_number[i]:
+                    cows += 1
+                elif cislo in hidden_number:
+                    bulls += 1
+            if cows < 4:
+                print(f"{bulls} bulls, {cows} cows.")
+    else:
+        print(f"Correct, you've guessed the right number in {pokusy} guesses!")
+        stop_timer(start_time)
+        oddeleni_textu()
 
 def oddeleni_textu(oddelovac = '-', delka = 45):
     print(oddelovac * delka)
+
+def generator_cisla() -> list:
+    rozsah = list(range(10))
+    print(rozsah)
+    nahodne_cislo = list()
+    for i in range(4):
+        if i == 0:
+            nahodne_cislo.append(str(rozsah.pop(random.randrange(1,10))))
+        else:
+            index = 10 - i
+            nahodne_cislo.append(str(rozsah.pop(random.randrange(index))))
+    return nahodne_cislo
 
 def kontrola_vstupu(vst_list: list, vysledek_kontroly = False):
     if len(vst_list) != 4:
@@ -35,7 +61,12 @@ def kontrola_vstupu(vst_list: list, vysledek_kontroly = False):
     oddeleni_textu()
     return vysledek_kontroly
 
-
+def stop_timer(pocatek):
+    cas = time.time() - pocatek
+    sekundy = round(cas % 60)
+    minuty = int(cas // 60)
+    hodiny = int(minuty // 60)
+    print(f"It took {hodiny} hours, {minuty} minutes and {sekundy} seconds.")
 
 if __name__ == "__main__":
     main()
