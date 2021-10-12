d5a9cfa64069ea7b473bea32e76ffde4a50a1d18
czjined <czjined@gmail.com>
2021-10-12 14:00:47 +0200

    ConnectionResetError 10054 :o(

--- Engeto_pa_3_project/election_scraper.py
+++ Engeto_pa_3_project/election_scraper.py
@@ -4,9 +4,8 @@ from bs4 import BeautifulSoup
 import sys
 
 
-def input_check() -> bool:
-    input_list = sys.argv
-    if len(input_list) != 3:
+def input_check(list_for_check: list) -> bool:
+    if len(list_for_check) != 3:
         text_for_stop = 'Three arguments expecting!'
         script_stop(text_for_stop)
     else:
@@ -14,9 +13,9 @@ def input_check() -> bool:
         return True
 
 
-def request_site() -> str:
+def request_site(html_argument: str) -> str:
     try:
-        home_site = requests.get('https://volby.cz/pls/ps2017nss/ps3?xjazyk=CZ')
+        home_site = requests.get(html_argument)
         home_site.raise_for_status()
     except HTTPError as http_err:
         text_for_stop = 'HTTP error occurred: {}'.format(http_err)
@@ -35,6 +34,10 @@ def script_stop(stop_text: str):
 
 
 if __name__ == '__main__':
-    if input_check():
-        req_site = request_site()
-        print(type(req_site))
+    input_list = ['SYS', 'https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7103', 'vysledky_prostejov.csv']
+    # input_list = sys.argv()
+    if input_check(input_list):
+        req_site = request_site(input_list[1])
+        soup_site = BeautifulSoup(req_site, 'html.parser')
+        print(soup_site.find_all('h3'), '\n')
+
